066c3c93d5a1d9e99109962e55f46647817a68a2
omar <abukarakiomar@gmail.com>
2023-07-25 22:03:50 +0300

    create edit note screen

--- lib/component/custom_app_bar.dart
+++ lib/component/custom_app_bar.dart
@@ -3,24 +3,36 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({
     super.key,
+    this.text,
+    this.icon,
+    this.onPressed,
   });
+  final String? text;
+  final Icon? icon;
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       elevation: 0,
-      title: const Text('Notes'),
+      title: Text(text ?? 'Notes'),
       actions: [
         Container(
             margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(18),
                 color: Colors.grey.shade800),
-            child:
-                IconButton(onPressed: () {}, icon: const Icon(Icons.search))),
+            child: IconButton(
+                onPressed: onPressed,
+                icon: icon ??
+                    const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ))),
       ],
     );
   }
 
+  @override
   Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height);
 }
---
+++ lib/views/edit_note_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/component/custom_app_bar.dart';
+import 'package:note_app/component/custom_text_field.dart';
+
+class EditNotePage extends StatelessWidget {
+  const EditNotePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CustomAppBar(
+          text: 'Edit Note',
+          icon: Icon(
+            Icons.done,
+            color: Colors.white,
+          )),
+      body: Column(children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: CustomTextField(),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: CustomTextField(
+            hintText: 'Contact',
+            hightOfTextFiled: 60,
+          ),
+        )
+      ]),
+    );
+  }
+}
