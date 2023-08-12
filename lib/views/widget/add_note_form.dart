9d7e924b77bb896ab2673072a29b804e1dd2c792
omar <abukarakiomar@gmail.com>
2023-08-12 15:12:00 +0300

    create color list

--- lib/views/widget/add_note_form.dart
+++ lib/views/widget/add_note_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 
+import 'colors_list_view.dart';
 import 'custom_add_note_button.dart';
 import 'custom_text_field.dart';
 
@@ -46,6 +47,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
               hintText: 'Content',
             ),
           ),
+          const ColorListView(),
           BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (context, state) {
               return CustomAddNoteButton(
---
+++ lib/views/widget/colors_list_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircleAvatar(radius: 40, backgroundColor: Colors.amber);
+  }
+}
+
+class ColorListView extends StatelessWidget {
+  const ColorListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const ColorItem();
+        },
+      ),
+    );
+  }
+}
